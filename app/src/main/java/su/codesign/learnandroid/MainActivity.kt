4ca63c6c4f6f3e08a10dfc8f019ab7360c175bdc
sergeiivanow <38918636+sergeiivanow@users.noreply.github.com>
2022-06-08 03:39:44 +0300

    p0031firstproject

---
+++ app/src/main/java/su/codesign/learnandroid/MainActivity.kt
@@ -0,0 +1,11 @@
+package su.codesign.learnandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
