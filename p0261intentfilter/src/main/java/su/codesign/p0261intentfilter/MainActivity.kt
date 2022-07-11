0ec6ac7756cf56b1ce47b9b474e5fa3e555851d3
Sergei Ivanov <38918636+sergeiivanow@users.noreply.github.com>
2022-07-11 21:00:02 +0300

    Add intents

--- p0261intentfilter/src/main/java/su/codesign/p0261intentfilter/MainActivity.kt
+++ p0261intentfilter/src/main/java/su/codesign/p0261intentfilter/MainActivity.kt
@@ -1,11 +1,26 @@
 package su.codesign.p0261intentfilter
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val date = findViewById(R.id.date) as Button
+        val time = findViewById(R.id.time) as Button
+
+        date.setOnClickListener {
+            val intent = Intent("su.codesign.action.DATE")
+            startActivity(intent)
+        }
+
+        time.setOnClickListener {
+            val intent = Intent("su.codesign.action.TIME")
+            startActivity(intent)
+        }
     }
 }
---
+++ p0261intentfilter/src/main/java/su/codesign/p0261intentfilter/TimeActivity.kt
@@ -0,0 +1,11 @@
+package su.codesign.p0261intentfilter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TimeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.time)
+    }
+}
