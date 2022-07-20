5ba4950b537c883199044193e603b0c176d30f5a
Sergei Ivanov <38918636+sergeiivanow@users.noreply.github.com>
2022-07-21 00:36:10 +0300

    Activity for result

--- p0261intentfilter/src/main/java/su/codesign/p0261intentfilter/MainActivity.kt
+++ p0261intentfilter/src/main/java/su/codesign/p0261intentfilter/MainActivity.kt
@@ -1,11 +1,22 @@
 package su.codesign.p0261intentfilter
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
+
+    var resultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            Toast.makeText(this, result.data?.getStringExtra("title"), Toast.LENGTH_SHORT).show()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,12 +27,11 @@ class MainActivity : AppCompatActivity() {
         date.setOnClickListener {
             val intent = Intent("su.codesign.action.DATE")
             intent.putExtra("title", "Date")
-            startActivity(intent)
         }
 
         time.setOnClickListener {
             val intent = Intent("su.codesign.action.TIME")
-            startActivity(intent)
+            resultLauncher.launch(intent)
         }
     }
 }
--- p0261intentfilter/src/main/java/su/codesign/p0261intentfilter/TimeActivity.kt
+++ p0261intentfilter/src/main/java/su/codesign/p0261intentfilter/TimeActivity.kt
@@ -1,7 +1,9 @@
 package su.codesign.p0261intentfilter
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import java.text.SimpleDateFormat
 import java.sql.Date;
@@ -11,8 +13,15 @@ class TimeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.time)
         val timeText = findViewById(R.id.timeText) as TextView
+        val backBtn = findViewById(R.id.back) as Button
         val sdf = SimpleDateFormat("HH:mm:ss")
         val currentTime = sdf.format(Date(System.currentTimeMillis()))
         timeText.text = currentTime
+
+        backBtn.setOnClickListener() {
+            intent.putExtra("title", "Time")
+            setResult(Activity.RESULT_OK, intent)
+            finish()
+        }
     }
 }
